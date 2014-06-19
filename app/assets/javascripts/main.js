946b40800c5592b819b61d49a620816f2fe6bb20
chuy2008 <chuylozano@ieee.org>
2014-06-18 19:03:56 -0500

    init

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,13 +1,13 @@
-// get name from service, name is entered in the app
+
 var name = request.get("name");
  
-// get and format the time
+
 var now = moment().format("dddd, MMMM D YYYY, h:mm:ss a");
  
-// put together the greeting string
+
 var greetingString = "Hello " + name + ". It is " + now;
  
-// format as JSON and return the response
+
 response.success(JSON.stringify({
    greeting: greetingString
  }), "application/json");
